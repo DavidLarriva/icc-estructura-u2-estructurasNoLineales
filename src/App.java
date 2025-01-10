5dbe477be230e4f2ce657c3ada27cf14538ba236
David Larriva <dalarrivac@gmail.com>
2025-01-10 12:59:53 -0500

    Ejercicio 3 en proceso

--- src/App.java
+++ src/App.java
@@ -1,10 +1,24 @@
 import main.Materia.Controllers.ArbolBinario;
 import main.Materia.Controllers.ArbolRecorridos;
-import main.Ejercicio_03_listLevels.*;
+import main.Ejercicio_03_listLevels.ListLevels;
 
 public class App {
     public static void main(String[] args) throws Exception {
         runArbolBinario();
+
+
+        public void runEjercicio3() {
+            ArbolBinario arbolBinario = new ArbolBinario();
+            ListLeves listLeves = new ListLeves();
+            int[] valores = {40, 20, 60, 10, 30, 50, 70, 5, 15, 55};
+            
+            for (int valor : valores) {
+                arbolBinario.insert(valor);
+            }
+    
+            System.out.println("Listas por Nivel:");
+            listLeves.imprimirListasPorNivel(listLeves.ListLeves(arbolBinario.getRoot()));
+        }
     
     }
 
@@ -30,20 +44,15 @@ public class App {
         System.out.println();
         arbolBinario.printTree();
 
-        private static void runEjercicio3() {
-            ArbolBinario arbolBinario = new ArbolBinario();
-            ListLeves listLeves = new ListLeves();
-            int[] valores = {valores};
-    
-            for (int i = 0; i < valores.length; i++){
-                arbolBinario.insert(valores[i]);
-                }
-            arbolBinario.printTree();
-            
-        }
+        
+
+        
     }
+
     
     }
+    
+    
 
 
     
--- src/main/Ejercicio_03_listLevels/ListLevels.java
+++ src/main/Ejercicio_03_listLevels/ListLevels.java
@@ -1,13 +1,49 @@
 package main.Ejercicio_03_listLevels;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import main.Materia.Models.Node;
 
 public class ListLevels {
-    public List<List<Node>> listLeves(Node root) {
-        if(root != null) {
-            return null;
+    public List<LinkedList<Node>> ListLeves(Node root) {
+        List<LinkedList<Node>> resultado = new ArrayList<>();
+
+        if (root == null) {
+            return resultado; 
+        }
+
+        LinkedList<Node> nivelActual = new LinkedList<>();
+        nivelActual.add(root);
+
+        while (!nivelActual.isEmpty()) {
+            resultado.add(new LinkedList<>(nivelActual)); 
+            LinkedList<Node> nivelSiguiente = new LinkedList<>();
+            
+            for (Node nodo : nivelActual) {
+                if (nodo.getLeft() != null) {
+                    nivelSiguiente.add(nodo.getLeft());
+                }
+                if (nodo.getRight() != null) {
+                    nivelSiguiente.add(nodo.getRight());
+                }
+            }
+            nivelActual = nivelSiguiente; 
+        }
+
+        return resultado;
+    }
+
+    public void imprimirListasPorNivel(List<LinkedList<Node>> listas) {
+        int nivel = 0;
+        for (LinkedList<Node> lista : listas) {
+            System.out.print("Nivel " + nivel++ + ": ");
+            for (Node nodo : lista) {
+                System.out.print(nodo.getValue() + " -> ");
+            }
+            System.out.println("null");
         }
     }
 }
+
