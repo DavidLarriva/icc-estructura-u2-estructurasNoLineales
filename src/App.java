4568797c40261928da9033522b25dcaa62a0ad21
David Larriva <dalarrivac@gmail.com>
2025-01-11 20:30:26 -0500

    Ejercicio completado

--- src/App.java
+++ src/App.java
@@ -6,19 +6,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         runArbolBinario();
 
+        System.out.println();
+        System.out.println();
 
-        /*public void runEjercicio3() {
-            ArbolBinario arbolBinario = new ArbolBinario();
-            ListLeves listLeves = new ListLeves();
-            int[] valores = {40, 20, 60, 10, 30, 50, 70, 5, 15, 55};
-            
-            for (int valor : valores) {
-                arbolBinario.insert(valor);
-            }
-    
-            System.out.println("Listas por Nivel:");
-            listLeves.imprimirListasPorNivel(listLeves.ListLeves(arbolBinario.getRoot()));
-        }*/
+        runEjercicio3();
+
+
+        
     
     }
 
@@ -47,6 +41,19 @@ public class App {
         
 
         
+    }
+
+    public static void runEjercicio3() {
+        ArbolBinario arbolBinario = new ArbolBinario();
+        ListLevels listLeves = new ListLevels();
+        int[] valores = {40, 20, 60, 10, 30, 50, 70, 5, 15, 55};
+        
+        for (int valor : valores) {
+            arbolBinario.insert(valor);
+        }
+
+        System.out.println("Listas por Nivel:");
+        listLeves.imprimirListasPorNivel(listLeves.ListLeves(arbolBinario.getRoot()));
     }
 
     
